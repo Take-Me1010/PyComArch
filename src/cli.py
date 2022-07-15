a16e6c16b0b6a135970e07cee7149d033f27f352
Take-Me1010 <take_ohmi@icloud.com>
2022-07-15 17:36:11 +0900

    initial commit 2

---
+++ src/cli.py
@@ -0,0 +1,70 @@
+
+import argparse
+from dataclasses import dataclass
+import logging
+from pathlib import Path
+from typing import List, Tuple
+
+from .logger import Logger
+logger = Logger("zipper")
+from . import zipper
+
+@dataclass
+class Args:
+    src: Path
+    dest: Path
+    include_patterns: List[str]
+    exclude_patterns: List[str]
+    recursive: bool
+    
+    class InValidArgumentsError(Exception):
+        pass
+    
+    def validate(self) -> Tuple[bool, str]:
+        ext = self.dest.suffix
+        if ext != ".zip":
+            return False, f"dest must be .zip file, not {ext}"
+        
+        return True, ""
+    
+    def raiseError(self, reason: str):
+        raise self.InValidArgumentsError(reason)
+
+def parse() -> Args:
+    parser = argparse.ArgumentParser(
+        description="A CLI tool to zip with useful options"
+    )
+    
+    parser.add_argument("dest", type=Path, help="a destination file name")
+    parser.add_argument("src", type=Path, help="a src file or directory to zip")
+    parser.add_argument("--include-patterns", nargs="*", help="a list of patterns to include files or directories even if it matches exclude_patterns. You can use any rule supported by re module.")
+    parser.add_argument("--exclude-patterns", nargs="*", help="a list of patterns to ignore files or directories. You can use any rule supported by re module.")
+    parser.add_argument("-r", "--recursive", action="store_true", help="determine if zip directory recursively.")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    
+    namespace = parser.parse_args()
+    
+    logger.setLevel(logging.DEBUG if namespace.verbose else logging.INFO)
+    
+    args = Args(
+        namespace.src, namespace.dest,
+        namespace.include_patterns, namespace.exclude_patterns,
+        namespace.no_recursive
+    )
+    
+    isvalid, reason = args.validate()
+    if not isvalid:
+        logger.error(reason)
+        args.raiseError(reason)
+    
+    
+    return args
+
+def main():
+    args = parse()
+    zipper.zip_files(
+        args.src, args.dest, args.include_patterns, args.exclude_patterns, recursive=args.recursive
+    )
+
+if __name__ == '__main__':
+    main()
---
+++ src/zipper.py
@@ -0,0 +1,43 @@
+
+from pathlib import Path
+from typing import List
+import zipfile
+import re
+
+def is_match(name: str, patterns: List[str]):
+    for pattern in patterns:
+        if re.match(pattern, name):
+            return True
+    
+    return False
+
+def is_exclude(name: str, include_patterns: List[str], exclude_patterns: List[str]) -> bool:
+    include = is_match(name, include_patterns)
+    exclude = is_match(name, exclude_patterns)
+    return not include and exclude
+
+def get_files_from_directory_to_zip(root: Path, include_patterns: List[str], exclude_patterns: List[str], recursive: bool = True) -> list[Path]:
+    result: list[Path] = []
+    for f in root.glob("*"):
+        if is_exclude(f.name, include_patterns, exclude_patterns):
+            continue
+
+        if f.is_dir() and recursive:
+            files = get_files_from_directory_to_zip(f, include_patterns, exclude_patterns, recursive)
+            result.extend(files)
+        
+        elif f.is_file():
+            result.append(f)
+    
+    return result
+
+def write(dest, files):
+    with zipfile.ZipFile(str(dest), mode="w") as zf:
+        for file in files:
+            zf.write(str(file))
+
+def zip_files(src: Path, dest: Path, include_patterns: List[str] = [], exclude_patterns: List[str] = [], recursive: bool = False):
+    files_to_zip = get_files_from_directory_to_zip(src, include_patterns, exclude_patterns, recursive)
+    # 相対パスじゃないと生成されるzipファイルがフルパスを反映してしまう
+    files_to_zip = [f.relative_to(src) for f in files_to_zip]
+    write(dest, files_to_zip)
